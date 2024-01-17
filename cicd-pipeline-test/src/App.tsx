c3e6cb71f096946c81595e5bcae1dbbfe71dfb11
Muhammad Hafizuddin <piz230601@gmail.com>
2024-01-17 11:48:52 +0800

    add dockerfile and adjust view

--- cicd-pipeline-test/src/App.tsx
+++ cicd-pipeline-test/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import * as react from "react-icons/fa";
+export default function App() {
+	return (
+		<>
+			<div className="h-fit">
+				<div className="max-w-xl mx-auto my-20">
+					<Card className="text-center shadow-md">
+						<CardHeader>
+							<CardTitle className="text-5xl">Yeah</CardTitle>
+							<CardDescription className="text-2xl">Success</CardDescription>
+						</CardHeader>
+						<CardContent>
+							<react.FaReact className="h-20 w-20 mx-auto" />
+						</CardContent>
+					</Card>
+				</div>
+			</div>
+		</>
+	);
 }
-
-export default App
